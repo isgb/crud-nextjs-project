53401b59a7aca7540e57a7ce7e8d7cc72c4f7fa3
isaias_pruebas <isgb_codigo@hotmail.com>
2024-06-10 23:30:50 -0600

    avances

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,13 +1,36 @@
 import { NextResponse } from "next/server";
+import {Prisma} from '@/libs/prisma';
 
-export function GET(request, {params}){
+export async function GET(request, {params}){
+    // console.log(params.id)
+   const task = await Prisma.task.findUnique({
+        where:{
+            id: Number(params.id)
+        }
+    })
+    console.log(task)
     return NextResponse.json("obteniendo tarea " + params.id)
 }
 
-export function PUT(request, {params}){
-    return NextResponse.json("actualizando tarea " + params.id)
+export async function PUT(request, {params}){
+    const data = await request.json()
+    const taskUpdate = await Prisma.task.update({
+        where:{
+            id: Number(params.id)
+        },
+        data:data
+    })
+    return NextResponse.json(taskUpdate)
 }
 
-export function DELETE(request, {params}){
-    return NextResponse.json("eliminando tarea " + params.id)
+export async function DELETE(request, {params}){
+    try {
+        const taskRemoved = await Prisma.task.delete({
+            where:{ id: Number(params.id)}
+        })
+        // console.log(taskRemoved);
+        return NextResponse.json(taskRemoved)
+    } catch (error) {
+        return NextResponse.json(error.message)
+    }
 }
