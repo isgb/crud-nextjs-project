355b8a28307641066fb9b4aa9c1bc7bdf69d834c
isaias_pruebas <isgb_codigo@hotmail.com>
2024-06-15 15:00:25 -0600

    debug

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,6 +7,8 @@ const AboutPage = () => {
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit necessitatibus aliquid a libero magni cupiditate obcaecati, 
        maiores labore, quibusdam officiis excepturi veniam alias officia, quas assumenda rerum nostrum ea dicta.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit necessitatibus aliquid a libero magni cupiditate obcaecati, 
        maiores labore, quibusdam officiis excepturi veniam alias officia, quas assumenda rerum nostrum ea dicta.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit necessitatibus aliquid a libero magni cupiditate obcaecati, 
+       maiores labore, quibusdam officiis excepturi veniam alias officia, quas assumenda rerum nostrum ea dicta.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit necessitatibus aliquid a libero magni cupiditate obcaecati, 
+       maiores labore, quibusdam officiis excepturi veniam alias officia, quas assumenda rerum nostrum ea dicta.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit necessitatibus aliquid a libero magni cupiditate obcaecati, 
        maiores labore, quibusdam officiis excepturi veniam alias officia, quas assumenda rerum nostrum ea dicta.</p>
   </section>
   )
